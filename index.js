e78e40d2f9f896789888b99e59ff2f7316bdc00a
Juan Claudio Caires Pereira <juanccaires@gmail.com>
2025-03-26 17:27:48 -0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
 
 var app = express();
 
+app.use(multer().any());
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", function (req, res) {
+  const file = req.files[0];
+  if (!file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
 
-
+  const { originalname, mimetype, size } = file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
